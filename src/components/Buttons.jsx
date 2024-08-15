9f215d0b234f35dc6c7ae4cdaeb72fc7b1653b8d
Siyasanga <siyasanga84@gmail.com>
2024-08-15 17:54:49 +0200

    refecter code reducing repetition

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import Chat from '../assets/Button/ChatsCircle.svg'
-import Hammer from '../assets/Button/HammerIcon.svg'
-import Hand from '../assets/Button/HandDepositIcon.svg'
+import React from 'react';
+import Chat from '../assets/Button/ChatsCircle.svg';
+import Hammer from '../assets/Button/HammerIcon.svg';
+import Hand from '../assets/Button/HandDepositIcon.svg';
 
 // buttons for hero section
 const Buttons = () => {
   return (
-    <div className='absolute ml-[300px] mt-[-85px] grid gap-4 grid-cols-3 h-16'>
-
-      <button class="poppins-medium text-lg leading-[27px] text-[#39468C] bg-white p-4  inline-flex items-center w-72  rounded-xl gap-4">
-        <img src={Hammer} alt="Hammer icon" class="w-8 h-8 "/>
-          Reparatie melden
-      </button>
-
-      <button class="poppins-medium text-lg leading-[27px] text-[#39468C] bg-white p-4  inline-flex items-center w-72  rounded-xl gap-4">
-        <img src={Hand} alt="Hand icon" class="w-8 h-8 "/>
-          Huur betalen
+    <div className="absolute ml-[300px] mt-[-85px] grid gap-4 grid-cols-3 h-16">
+      {buttonsData.map((button, index) => (
+        <button
+          key={index}
+          className="poppins-medium text-lg leading-[27px] text-[#39468C] bg-white p-4 inline-flex items-center w-72 rounded-xl gap-4"
+        >
+          <img src={button.icon} alt={button.alt} className="w-8 h-8" />
+          {button.label}
         </button>
-
-      <button class="poppins-medium text-lg leading-[27px] text-[#39468C] bg-white p-4  inline-flex items-center w-72  rounded-xl gap-4">
-        <img src={Chat} alt="Chat icon" class="w-8 h-8 "/>
-          Contact
-      </button>
+      ))}
     </div>
-  )
-}
+  );
+};
+// button data
+const buttonsData = [
+  {
+    icon: Hammer,
+    alt: "Hammer icon",
+    label: "Reparatie melden",
+  },
+  {
+    icon: Hand,
+    alt: "Hand icon",
+    label: "Huur betalen",
+  },
+  {
+    icon: Chat,
+    alt: "Chat icon",
+    label: "Contact",
+  },
+];
 
-export default Buttons
+export default Buttons;
