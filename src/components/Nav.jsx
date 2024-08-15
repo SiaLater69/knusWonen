ff2dae28da28ca59a8e1f1cd71f87ada0b2e321a
Siyasanga <siyasanga84@gmail.com>
2024-08-15 23:07:15 +0200

    make nav responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.svg';
 import Frame3 from '../assets/Frame-3.svg';
 import Frame4 from '../assets/Frame-4.svg';
 
 const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between items-center bg-white h-20 w-[1512px]">
+    <nav className="flex flex-wrap justify-between items-center bg-white p-4 w-full">
       {/* Logo */}
-      <div className="ml-[143px] mt-5">
-        <img src={logo} alt="Logo" className="h-10 w-52" />
+      <div className="flex items-center lg:ml-[197px]">
+        <img src={logo} alt="Logo" className="h-8 w-auto" />
       </div>
 
-      {/* Middle links */}
-      <div className="poppins-semibold text-base leading-6 text-[#39468C] flex mt-6 gap-x-3">
-        <a href="#">Ik huur</a>
-        <a href="#">Ik zoek</a>
-        <a href="#">Over ons</a>
-        <a href="#">Projecten</a>
-      </div>
+      {/* Hamburger menu for mobile */}
+      <button
+  aria-label="Toggle navigation menu"
+  aria-expanded={isMenuOpen}
+  aria-controls="nav-menu"
+  className="lg:hidden"
+  onClick={() => setIsMenuOpen(!isMenuOpen)}
+>
+  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+  </svg>
+</button>
 
-      {/* Right icons */}
-      <div className="flex mr-[220px] gap-x-2.5 m-2.5 mt-6">
-        <a href="#">
-          <img src={Frame3} alt="Logo" className="h-7 w-7" />
-        </a>
-        <a href="#">
-          <img src={Frame4} alt="Logo" className="h-7 w-7" />
-        </a>
-      </div>
+      {/* Navigation links and icons */}
+      <div className={`w-full lg:flex lg:items-center lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
+  <div className="text-sm lg:flex-grow lg:justify-center lg:ml-[-870px]">
+    <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-[#39468C] hover:text-blue-700 mr-4 poppins-semibold">
+      Ik huur
+    </a>
+    <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-[#39468C] hover:text-blue-700 mr-4 poppins-semibold">
+      Ik zoek
+    </a>
+    <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-[#39468C] hover:text-blue-700 mr-4 poppins-semibold">
+      Over ons
+    </a>
+    <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-[#39468C] hover:text-blue-700 poppins-semibold">
+      Projecten
+    </a>
+  </div>
+  <div className="flex items-center mt-4 lg:mr-36">
+    <a href="#" className="mr-2">
+      <img src={Frame3} alt="Icon 1" className="h-6 w-6" />
+    </a>
+    <a href="#">
+      <img src={Frame4} alt="Icon 2" className="h-6 w-6" />
+    </a>
+  </div>
+</div>
     </nav>
   );
 };
